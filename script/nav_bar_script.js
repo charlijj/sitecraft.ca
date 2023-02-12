8f791ba9e1039245d0b475738f6efed6de7bb2c7
charlijj <jjcharlinski@gmail.com>
2023-02-11 22:14:03 -0800

    added navbar script

---
+++ script/nav_bar_script.js
@@ -0,0 +1,28 @@
+function navBarChange () {
+
+    let nav = document.getElementById(`navBar`);
+    let rect = document.getElementById(`top`).getBoundingClientRect();
+
+    nav.style.opacity = `0.5`;
+    nav.style.height = `33px`;
+
+    if (rect.top < 0)
+    {
+        nav.style.position = `fixed`;
+
+    }
+    else if (rect.top == 0)
+    {
+        nav.style.position = `static`;
+
+    }
+
+    setTimeout(() => {
+        
+        nav.style.opacity = `1`; 
+        nav.style.height = `55px`;
+        nav.style.marginBottom = '40px';     
+
+    }, 500);
+
+}
